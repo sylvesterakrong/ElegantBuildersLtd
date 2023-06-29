c230ee4ba3eedf5176360213a4c404e64be72b12
sylvesterakrong <akrongsylvester@gmail.com>
2023-06-29 19:32:54 +0000

    new

---
+++ elegantbuildersltd/src/app/components/Services.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Services = () => {
+    return(
+        <>
+            <div className="px-5 py-20 bg-black grid gap-4 grid-cols-2 grid-rows-1 justify-center text-3xl text-gray-450 h-[50rem] items-center">
+                <div>
+                    <p>
+                    We offer a wide range of services to our clients which meets the needs and requirements. Many of which are essential and meet top-class requirements. 
+                    </p>
+                </div>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+        
+        </>
+    );
+}
+
+export default Services
