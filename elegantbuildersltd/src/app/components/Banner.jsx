5d5b0383f150179a58d75aeea6e3357d276c8cd7
sylvesterakrong <akrongsylvester@gmail.com>
2023-06-30 00:59:34 +0000

    update

--- elegantbuildersltd/src/app/components/Banner.jsx
+++ elegantbuildersltd/src/app/components/Banner.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Banner = () => {
--- elegantbuildersltd/src/app/components/Services.jsx
+++ elegantbuildersltd/src/app/components/Services.jsx
@@ -10,7 +10,7 @@ const Services = () => {
                     </p>
                 </div>
                 <div>
-                    <p></p>
+                    
                 </div>
             </div>
         
